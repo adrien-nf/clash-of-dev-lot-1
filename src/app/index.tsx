917922ded8faebb6011835cc0921c392741d3140
Adrien N. F <iamnautilusjungle@gmail.com>
2024-09-17 11:50:33 +0200

    feat: removed storybook and structured project

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
-function App() {
+export const App = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -30,6 +29,4 @@ function App() {
       </p>
     </>
   );
-}
-
-export default App;
+};
