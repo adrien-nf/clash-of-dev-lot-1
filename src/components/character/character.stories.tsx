d993dfb93ca9c642a2ecd38b0e3c0d72facbbe17
Adrien N. F <iamnautilusjungle@gmail.com>
2024-09-22 22:52:00 +0200

    feat: fix for build

--- src/components/character/character.stories.tsx
+++ src/components/character/character.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Character } from "./character";
 import image from "../../assets/characters/artist.png";
-import PointerIcon from "../icons/pointer-icon";
+import { PointerIcon } from "../icons/pointer-icon";
 
 const meta = {
   component: Character,
